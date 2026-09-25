532cba589c041da9244bab33055498ea8315ec5d
herrjase <gerrjase@gmail.com>
2026-09-26 01:22:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,17 +4,20 @@ import discord
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
+intents.members = True
 
 client = discord.Client(intents=intents)
+
 
 @client.event
 async def on_ready():
     print(f"Бот запущен: {client.user}")
 
     for guild in client.guilds:
-        print(f"\nСервер: {guild.name}")
+        print(f"Сервер: {guild.name}")
 
         for role in guild.roles:
-            print(f"{role.name} → {role.id}")
+            print(f"РОЛЬ: {role.name} | ID: {role.id}")
+
 
 client.run(TOKEN)
